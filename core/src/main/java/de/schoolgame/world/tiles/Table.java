6147484f8023cbcea331a32cefae6560da611645
michirando <215148866+michirando@users.noreply.github.com>
2025-07-22 02:04:31 +0200

    bug fix

--- core/src/main/java/de/schoolgame/world/tiles/Table.java
+++ core/src/main/java/de/schoolgame/world/tiles/Table.java
@@ -1,9 +1,11 @@
 package de.schoolgame.world.tiles;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 import de.schoolgame.primitives.Vec2f;
 import de.schoolgame.primitives.Vec2i;
+import de.schoolgame.render.texture.TileSet;
 import de.schoolgame.state.GameState;
 import de.schoolgame.world.Tile;
 
@@ -11,8 +13,12 @@ public class Table extends Tile {
     @Override
     public void render(Batch batch, Vec2f drawPosition, Vec2i worldPosition) {
         var state = GameState.INSTANCE;
+
         int tileSize = state.world.getTileSize();
-        Texture texture = state.assetManager.get("tiles/table/table", Texture.class);
-        batch.draw(texture, drawPosition.x, drawPosition.y, tileSize, tileSize);
+        byte connections = state.world.connectionsAt(worldPosition);
+        TileSet tileSet = state.assetManager.get("tiles/table/table", TileSet.class);
+        TextureRegion textureRegion = tileSet.getTextureRegion(connections);
+
+        batch.draw(textureRegion, drawPosition.x, drawPosition.y, tileSize, tileSize);
     }
 }
